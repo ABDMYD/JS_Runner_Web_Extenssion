9f7cc463ec37a266d4cca14b35e2ba720ac65cad
ABDMYD <30744598+ABDMYD@users.noreply.github.com>
2019-06-12 21:21:33 +0300

    Create Test_JS_File.js

---
+++ Test_JS_File.js
@@ -0,0 +1,15 @@
+function create(element, content){
+  var ele = document.createElement(element);
+  ele.innerHTML = content;
+  document.body.appendChild(ele);
+  return ele;
+}
+
+let H1 = create("h1", "Hello world!"),body = document.body;
+H1.style.color = "#fff";
+H1.style.fontFamily = "Arial";
+H1.style.position = "absolute";
+H1.style.left = "50%";
+H1.style.top = "50%";
+H1.style.transform = "translate(-50%,-50%)"{;
+body.style.background = "#333";
